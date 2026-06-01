27a8f6e3f3f2d002689bf4cddcb46552e13845b7
sky-just <2528155676@qq.com>
2026-06-02 02:13:49 +0800

    Update listeners.js

--- listeners.js
+++ listeners.js
@@ -69,7 +69,7 @@ function setupEventListeners() {
         'custom-replies-function': 'custom-replies-modal',
         'music-player-toggle': null,
         'stats-function': 'stats-modal',
-        'decision-function': 'decision-menu-modal',
+        'decision-function': 'decision-modal',
         'fortune-lenormand-function': 'fortune-lenormand-modal',
         'anniversary-function': 'anniversary-modal',
         'mood-function': 'mood-modal',
